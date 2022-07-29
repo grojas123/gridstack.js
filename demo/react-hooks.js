bf4ced2ce8c515d7cf2ff7cf868ac97c4ddecd57
GusRojas <15184392+grojas123@users.noreply.github.com>
2022-07-29 13:28:04 -0600

    Changed the var grid=document.querySelector('.grid-stack').gridstack;

--- demo/react-hooks.js
+++ demo/react-hooks.js
@@ -1,39 +1,38 @@
-// This example is a simpler demo to demostrate how use the hook with React 18.2.0 and gridstack.js
-// Is a demo how you can setup the hook and how update the grid after the app started .
-
 import React, {useEffect} from "react";
 
 import { GridStack } from 'gridstack';
 import 'gridstack/dist/gridstack.css';
-
+import 'gridstack/dist/gridstack-extra.css';
 import "./App.css";
 
 function App() {
     let serializedData = [
-        {x: 0, y: 0, w: 1, h: 1,autoPosition:false,content: 'item 1'},
+        {x: 1, y: 0, w: 1, h: 1,autoPosition:false,content: 'item 1'},
         {x: 0, y: 2, w: 1,h: 1,autoPosition:false, content: 'item 2'},
         {x: 0, y: 3,w:1,h: 1,autoPosition:false,content: 'item 3'}
     ];
 
     useEffect(() => {
-        var grid = GridStack.init();
+        var grid = GridStack.init({column: 10,maxRow:10});
 
     });
 
 
 function onClick(){
-    var grid = GridStack.init();
+    var grid=document.querySelector('.grid-stack').gridstack;
     grid.removeAll()
-    console.log("run useEffect")
     grid.load(serializedData)
 
 }
     function onClickv2(){
-        var grid = GridStack.init();
+        let grid=document.querySelector('.grid-stack').gridstack;
+        
         grid.addWidget({x: 0, y: 4,w:1,h: 1,content: 'item 4'});
         grid.addWidget({x: 0, y: 5,w:1,h: 1,content: 'item 5'});
+        
 
     }
+ 
     return (
         <div className="App">
             {<button onClick={()=>onClick()}>Click me</button>}
@@ -42,6 +41,7 @@ function onClick(){
 
             </div>
         </div>
+
     );
 }
 
